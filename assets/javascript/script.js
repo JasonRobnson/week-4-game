759b92669756aa1ec9ab60a0a7448304223d63c8
unknown <bubbastinx@gmail.com>
2018-03-09 13:58:33 -0600

    Javascript: game random number generator working

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+'strict'
+
+var crysPlayerGame = {
+    playerNumber: [],
+    playerwIN: [],
+    playerLoss: [],
+    crysOne: [],
+    crysTwo: [],
+    crysThree: [],
+}
+
+//returns a random number between 19 and 120
+function gameRandomNumGen (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(gameRandomNumGen(19, 120)); 
+
+
+
+
+
+
+
