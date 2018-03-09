ef54de6f63f83b55fc2283d342f83ef51734e56d
unknown <bubbastinx@gmail.com>
2018-03-09 14:27:41 -0600

    added the first part of the crystal number generator for crystal one

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,15 +11,23 @@ var crysPlayerGame = {
 
 //returns a random number between 19 and 120
 function gameRandomNumGen (min, max) {
+    var min = 19;
+    var max = 121;
     return Math.floor(Math.random() * (max - min)) + min;
 }
-console.log(gameRandomNumGen(19, 121)); 
+console.log(gameRandomNumGen())
 
 function playerRandomNumGen (min, max) {
+    var min = 1;
+    var max = 13;
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-console.log(playerRandomNumGen(1, 13)); 
+
+var crystalOneNumber = playerRandomNumGen();
+    console.log(crystalOneNumber);
+   
+
 
 
 
